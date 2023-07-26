3abbab62fce1455747cf0dafbe4eab38debdad22
Zach Legein <zlegein@gmail.com>
2023-07-26 11:14:02 -0500

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -67,7 +67,7 @@ zdot="${ZDOTDIR:-$HOME}"
 ZSH="${ZSH:-$HOME/.oh-my-zsh}"
 
 # Default settings
-REPO=${REPO:-ohmyzsh/ohmyzsh}
+REPO=${REPO:-zlegein/ohmyzsh}
 REMOTE=${REMOTE:-https://github.com/${REPO}.git}
 BRANCH=${BRANCH:-master}
 

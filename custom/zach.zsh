7e4a740a9de3b3c751203c9413af8731a124c7b6
Legein, Zach (SP) <zach.legein@marriott-sp.com>
2024-03-05 15:08:49 -0600

    add mtu test command

--- custom/zach.zsh
+++ custom/zach.zsh
@@ -8,11 +8,12 @@ alias stash='git stash'
 alias unstash='git stash pop'
 alias gdiff='git diff'
 alias gpass='git config --global credential.helper osxkeychain'
+alias gprune='git fetch --prune origin && git '
 alias gsearch=searchGitLogs
 alias gdel=removeBranchesWithPrefix
 alias glist='git branch -r --merged develop | grep -E "(story|task[s]?|bugfix|feature|project|release)"'
 alias pulldx=refreshDXRepos
-alias dx=dxGoto
+alias mi=miGoto
 alias nocors='open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir="/tmp/chrome_dev_test" --disable-web-security'
 alias server='http-server --ssl --cert ~/workspace/hilton/dx/cert.pem --key ~/workspace/hilton/dx/cert.key'
 alias dxk='docker rm -f $(docker ps -a -fname=dx* -q)'
@@ -20,6 +21,9 @@ alias dxrmi='docker rmi -f $(docker images | grep "^<none>" | awk "{print $3}")'
 alias dxnuke='docker rm -f $(docker ps -a -q) && docker rmi -f $(docker images -q)'
 alias dxs='docker-compose -f dx-docker/compose/pot-nodejs-develop.yml up --build'
 alias hist='history | grep $1'
+alias gremove='git rebase -r --onto $1^ $1'
+alias aem='cd ~/workspace/marriott/aem-sdk/aem && java -Xmx2048M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=30303 -jar aem-author-p4502.jar -gui -r"author,localdev"' 
+alias mtu='ping -g 1350 -G 1520 -h 10 -D $1'
 
 # Additional Path Settings
 
@@ -31,8 +35,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+export VOLTA="$HOME/.volta"
+
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
 export PATH="$PATH:$HOME/.rvm/bin"
+export PATH="$PATH:$VOLTA/bin"
 
 export GEM_HOME=$HOME/.gem
 export PATH=$GEM_HOME/bin:$PATH
@@ -50,14 +57,14 @@ killOnPort() {
   fi
 }
 
-dxGoto() {
+miGoto() {
   if [ "$1" -eq '' ]; then
     echo "Heading over to funky town!"
   else
     echo "Heading over to $1 town!"
   fi
 
-  cd ~/workspace/hilton/dx/$1
+  cd ~/workspace/marriott/$1
 
 }
 
